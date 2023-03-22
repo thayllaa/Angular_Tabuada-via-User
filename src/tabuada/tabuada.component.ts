90af4799cf3fcf6b7f8fab7f213ef6e0351984f3
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-22 20:53:00 -0300

    1 file modified

--- src/tabuada/tabuada.component.ts
+++ src/tabuada/tabuada.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -10,11 +10,22 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./tabuada.component.css'],
 })
 export class TabuadaComponent implements OnInit {
-  calculate() {}
+  @Input() num: number = 0;
+  show: boolean = false;
+  counter = 0;
 
-  constructor() {
-    this.calculate;
-  }
+  constructor() {}
 
   ngOnInit() {}
+
+  calculate() {
+    var numTabuada = '';
+    var valor = this.num;
+    for(var count = 1; count <= 10; count++) {
+        numTabuada += 
+        this.num + ' X ' + count + ' = ' + Number(this.num) * count + `\n`;
+      }
+      return this.calculate;
+    }
+  }
 }
