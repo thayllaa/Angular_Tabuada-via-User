fbf242dfcb2b1360b5e0d21864cf151dbde3ab68
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-22 20:10:22 -0300

    1 file modified, 1 added

--- src/tabuada/tabuada.component.ts
+++ src/tabuada/tabuada.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tabuada',
   standalone: true,
+  imports: [ FormsModule ],
   templateUrl: './tabuada.component.html',
   styleUrls: ['./tabuada.component.css'],
 })
 export class TabuadaComponent implements OnInit {
 
-  
+
   constructor() {}
 
   ngOnInit() {}
