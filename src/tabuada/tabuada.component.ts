75474ffa6a121a64c11a7ce242892d83eaffc7f2
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-22 20:22:45 -0300

    1 file modified

--- src/tabuada/tabuada.component.ts
+++ src/tabuada/tabuada.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabuada',
   standalone: true,
-  imports: [ Comm, FormsModule ],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './tabuada.component.html',
   styleUrls: ['./tabuada.component.css'],
 })
